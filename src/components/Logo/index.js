2ef29130563381811299bbd92b218e9f3f06b474
RianFelixx <rfo@etepd.com>
2022-11-14 23:34:59 -0300

    Criando client e server. Movendo arquivos para o cliente

--- src/components/Logo/index.js
+++
@@ -1,9 +0,0 @@
-import './logo.css'
-import { Link } from 'react-router-dom'
-export function Logo() {
-    return (
-        <Link to="/">
-            <h1 className='logo'>Acesso<span className='logo-text'>Escola</span></h1>
-        </Link>
-    )
-}
